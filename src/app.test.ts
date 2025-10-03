92352ecb2b08b87b289a6e72c82a0b92ce21d1c2
Leonardo Valeriano <leonardo.valeriano.public@gmail.com>
2025-10-02 21:57:48 -0500

    test app.ts

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,33 @@
-describe("App", () => {
-  test("should be true", () => {
-    expect(true).toBe(true);
+// process.argv = ["node", "app.ts", "-b", "10"];
+
+// import "./app";
+import { ServerApp } from "./presentation/server-app";
+
+describe("test App.ts", () => {
+  test("should call Server.run with values", async () => {
+    const serverRunMock = jest.fn();
+    ServerApp.run = serverRunMock;
+    process.argv = [
+      "node",
+      "app.ts",
+      "-b",
+      "10",
+      "-l",
+      "5",
+      "-s",
+      "-n",
+      "test-file",
+      "-d",
+      "test-destination",
+    ];
+    await import("./app");
+
+    expect(serverRunMock).toHaveBeenCalledWith({
+      base: 10,
+      limit: 5,
+      showTable: true,
+      fileName: "test-file",
+      fileDestination: "test-destination",
+    });
   });
 });
